5d424756a96381c8858af5f660c99c36d4d16f5d
Aron Greenj <arongreen0501@gmail.com>
2017-04-17 19:47:05 +0000

    mappa yfir orders ur firebase

--- src/components/MyOrder.js
+++ src/components/MyOrder.js
@@ -1,28 +1,51 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 import firebase from 'firebase';
 import userRefFor from './userRef';
+import Spinner from './Spinner';
 
 class MyOrder extends Component {
+  state = {
+    myOrders: [],
+    loading: true,
+  }
+
   componentWillMount() {
     const { adversityId } = this.props.match.params;
     const userRef = userRefFor(this.props.user);
 
-    userRef.once('value').then(function(snapshot) {
+    userRef.child('orders').once('value').then((snapshot) => {
       console.log(snapshot.val());
-      // ...
+      this.setState({ myOrders: snapshot.val(), loading: false })
+      console.log(this.state);
     });
+
     // console.log(this.props.match.params);
   }
 
-  // userRef.once('value').then(function(snapshot) {
-  //   console.log(snapshot);
-  // }
+  renderOrder = () => {
+    if(this.state.loading === true) {
+      return (
+        <Spinner />
+      )
+    }
+
+    return _.map(this.state.myOrders, (myOrder, key) => {
+      return <li key={key}>{myOrder}</li>
+    });
+  }
 
   render() {
-    const { state } = this;
+    // const { state } = this;
 
     return (
-      <h1>My Order</h1>
+      <div>
+        <h1>My Order</h1>
+        <ul>
+          {this.renderOrder()}
+        </ul>
+      </div>
+
     );
   }
 
